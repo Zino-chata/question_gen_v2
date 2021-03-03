763a10838fda0955132c05335ccb8b9acbfe56cb
josh <joshsirota@gmail.com>
2021-03-03 22:25:29 +0000

    updates

---
+++ prepare_trainer.py
@@ -0,0 +1,22 @@
+from run_qg import run_qg
+
+args_dict = {
+    "model_name_or_path": "t5-small",
+    "model_type": "t5",
+    "tokenizer_name_or_path": "t5_qg_tokenizer",
+    "output_dir": "t5-small-e2e-qg-zino",
+    "train_file_path": "data/train_data_e2e_qg_t5.pt",
+    "valid_file_path": "data/valid_data_qg_t5.pt",
+    "per_device_train_batch_size": 32,
+    "per_device_eval_batch_size": 32,
+    "gradient_accumulation_steps": 8,
+    "learning_rate": 1e-4,
+    "num_train_epochs": 10,
+    "seed": 42,
+    "do_train": True,
+    "do_eval": True,
+    "evaluate_during_training": True,
+    "logging_steps": 100
+}
+
+run_qg(args_dict)
