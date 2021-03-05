fe0d83837c84a042ada0a634ac7e96d0b70d1361
Elozino Egonmwan <elozinoegonmwan@Elozinos-MacBook-Pro.local>
2021-03-05 12:28:19 -0700

    updates

--- run_qg.sh
+++ run_qg.sh
@@ -7,6 +7,8 @@ python run_qg.py \
     --max_target_length 32 \
     --model_name_or_path t5-small \
     --tokenizer_name_or_path t5_qg_tokenizer \
+    --train_file_path data/train_data.pt \
+    --valid_file_path data/valid_data.pt \
     --output_dir t5-small-e2e-qg-zino \
     --per_device_train_batch_size: 32 \
     --per_device_eval_batch_size 16 \
