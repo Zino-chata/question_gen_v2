60e3c0d7908ea83a12cde6b80bd7a0eae1ce055a
Elozino Egonmwan <elozinoegonmwan@Elozinos-MacBook-Pro.local>
2021-03-05 23:59:38 -0700

    updates

--- prepare_data.py
+++ prepare_data.py
@@ -220,7 +220,7 @@ def preprocess_data(data):
     target_text = [sub["target_text"] for sub in data_dict]
     task_text = [sub["task"] for sub in data_dict]
 
-    data_dict={"source_text":source_text[:1000], "target_text": target_text[:1000], "task": task_text[:1000]}
+    data_dict={"source_text":source_text, "target_text": target_text, "task": task_text}
     data_dict = nlp.Dataset.from_dict(data_dict)
 
     return data_dict
--- run_qg.sh
+++ run_qg.sh
@@ -10,13 +10,13 @@ python run_qg.py \
     --valid_file_path data/valid_data.pt \
     --output_dir t5-small-e2e-qg-zino \
     --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 16 \
+    --per_device_eval_batch_size 32 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-4 \
     --num_train_epochs 10 \
     --seed 42 \
     --do_train True \
-    --logging_steps 2 \
+    --logging_steps 100 \
     --logging_dir tensor_logs \
-    --save_total_limit 10 \
+    --save_total_limit 20 \
     --overwrite_output_dir True
